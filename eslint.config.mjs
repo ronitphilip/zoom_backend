4c3137278286cd404b02ead8a98544efe8173fb8
ronitphilip <ronitpc1819@gmail.com>
2025-06-05 20:44:34 +0530

    config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
+    ignores: ['jest.config.js'],
+  },
+  {
     files: ["**/*.{js,ts}"],
     languageOptions: {
       parser: tseslint.parser,
